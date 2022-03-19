4ac2a5840b0113035f69faed075a8a3e8310f407
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2022-03-19 16:52:24 +0200

    media: rkisp1: isp: Disallow multiple active sources
    The ISP supports multiple source subdevs, but can only capture from a
    single one at a time. The source is selected through link setup. The
    driver finds the active source in its .s_stream() handler using the
    media_entity_remote_pad() function. This fails to reject invalid
    configurations with multiple active sources. Fix it by using the
    media_entity_remote_source_pad() helper instead, and inline
    rkisp1_isp_get_source() in rkisp1_isp_s_stream() as the function is
    small and has a single caller.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- drivers/media/platform/rockchip/rkisp1/rkisp1-isp.c
+++ drivers/media/platform/rockchip/rkisp1/rkisp1-isp.c
@@ -57,20 +57,6 @@
  * Helpers
  */
 
-static struct v4l2_subdev *rkisp1_isp_get_source(struct v4l2_subdev *sd)
-{
-	struct media_pad *local, *remote;
-	struct media_entity *sensor_me;
-
-	local = &sd->entity.pads[RKISP1_ISP_PAD_SINK_VIDEO];
-	remote = media_entity_remote_pad(local);
-	if (!remote)
-		return NULL;
-
-	sensor_me = remote->entity;
-	return media_entity_to_v4l2_subdev(sensor_me);
-}
-
 static struct v4l2_mbus_framefmt *
 rkisp1_isp_get_pad_fmt(struct rkisp1_isp *isp,
 		       struct v4l2_subdev_state *sd_state,
@@ -742,6 +728,7 @@ static int rkisp1_isp_s_stream(struct v4l2_subdev *sd, int enable)
 	struct rkisp1_isp *isp = to_rkisp1_isp(sd);
 	struct rkisp1_device *rkisp1 = isp->rkisp1;
 	const struct rkisp1_sensor_async *asd;
+	struct media_pad *source_pad;
 	int ret;
 
 	if (!enable) {
@@ -753,10 +740,17 @@ static int rkisp1_isp_s_stream(struct v4l2_subdev *sd, int enable)
 		return 0;
 	}
 
-	rkisp1->source = rkisp1_isp_get_source(sd);
+	source_pad = media_pad_remote_pad(&isp->pads[RKISP1_ISP_PAD_SINK_VIDEO]);
+	if (IS_ERR(source_pad)) {
+		dev_dbg(rkisp1->dev, "Failed to get source for ISP: %ld\n",
+			PTR_ERR(source_pad));
+		return -EPIPE;
+	}
+
+	rkisp1->source = media_entity_to_v4l2_subdev(source_pad->entity);
 	if (!rkisp1->source) {
-		dev_warn(rkisp1->dev, "No link between isp and source\n");
-		return -ENODEV;
+		/* This should really not happen, so is not worth a message. */
+		return -EPIPE;
 	}
 
 	asd = container_of(rkisp1->source->asd, struct rkisp1_sensor_async,
