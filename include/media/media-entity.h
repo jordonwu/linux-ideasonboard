a3ba6bdc858c6a69bc08e657b557a72c181d6a62
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2022-06-15 04:11:10 +0900

    media: mc-entity: Add a new helper function to get a remote pad for a pad
    The newly added media_entity_remote_source_pad() helper function handles
    use cases where the entity has a link enabled uniqueness constraint
    covering all pads. There are use cases where the constraint covers a
    specific pad only. Add a new media_pad_remote_pad() function to handle
    this. It operates as media_entity_remote_source_pad(), but on a given
    pad instead of on the entity.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- include/media/media-entity.h
+++ include/media/media-entity.h
@@ -905,6 +905,24 @@ media_entity_remote_source_pad(const struct media_entity *entity)
 }
 
 /**
+ * media_pad_remote_pad - Find a remote pad connected to a pad
+ * @pad: The pad
+ *
+ * Search for and return a remote pad connected to @pad through an enabled
+ * link. If multiple (or no) remote pads are found, an error is returned.
+ *
+ * The uniqueness constraint makes this helper function suitable for entities
+ * that support a single active source at a time on a given pad.
+ *
+ * Return: A pointer to the remote pad, or one of the following error pointers
+ * if an error occurs:
+ *
+ * * -ENOTUNIQ - Multiple links are enabled
+ * * -ENOLINK - No connected pad found
+ */
+struct media_pad *media_pad_remote_pad(const struct media_pad *pad);
+
+/**
  * media_entity_is_streaming - Test if an entity is part of a streaming pipeline
  * @entity: The entity
  *
