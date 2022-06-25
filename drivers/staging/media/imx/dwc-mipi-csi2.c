bdd0b86c02b95ce277afcc391afee241f09465a0
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2022-06-25 18:02:24 +0100

    media: mc-entity: Rename media_entity_remote_pad() to media_pad_remote_pad_first()
    The media_entity_remote_pad() is misnamed, as it operates on a pad and
    not an entity. Rename it to media_pad_remote_pad_first() to clarify its
    behaviour.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Acked-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@kernel.org>
    (cherry picked from commit b2e44430b6348f68f56e78e932e6312f12128778)

--- drivers/staging/media/imx/dwc-mipi-csi2.c
+++ drivers/staging/media/imx/dwc-mipi-csi2.c
@@ -940,7 +940,7 @@ static struct media_pad *dwc_csi2_get_remote_sensor_pad(struct dwc_mipi_csi2_hos
 			sink_pad = &subdev->entity.pads[i];
 
 			if (sink_pad->flags & MEDIA_PAD_FL_SINK) {
-				source_pad = media_entity_remote_pad(sink_pad);
+				source_pad = media_pad_remote_pad_first(sink_pad);
 				if (source_pad)
 					break;
 			}
